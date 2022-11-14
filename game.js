1df33ccb1d82f1489af41f50164d1402febc8a20
Saurabh0713 <109545639+Saurabh0713@users.noreply.github.com>
2022-11-14 23:04:28 +0530

    1234

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
 
 let X = Math.floor(Math.random() * 10);
-
+let attempts =0;
 
 function handleClick(event){
     let numId = event.target.id;
     let num = parseInt(numId[3]);
+    attempts++;
 
     if(num==X){
         // console.log("right")
@@ -14,11 +15,25 @@ function handleClick(event){
     printAns.innerHTML = printAns.innerHTML+ans;
     }else{
         // console.log("wrong",X)
-        let ans = "<h1 id='result-head' style='color:red'>"+num+" is wrong, number was "+X+"</h1><button class='replay' type='submit' onclick='handleReplay()'>REPLAY</button>";
-        let printAns = document.getElementById("result");
-        printAns.style.display = "flex";
-        printAns.innerHTML = printAns.innerHTML+ans;
+        let wrongAttemt = "<h3 id='attemptsHeading'>Attempts Remaining: "+(3-attempts)+"</h3><button id='retry' onclick='handleRetry()' style='padding:20px'>RETRY</button>";
+        var showAttempts = document.getElementById("result");
+        showAttempts.style.display = "flex";
+        if(attempts!=3){
+            showAttempts.innerHTML = showAttempts.innerHTML+ wrongAttemt;
+        }
+
+        if(attempts==3){
+            let ans = "<h1 id='result-head' style='color:red'>"+num+" is wrong, number was "+X+"</h1><button class='replay' type='submit' onclick='handleReplay()'>REPLAY</button>";
+            let printAns = document.getElementById("result");
+            printAns.style.display = "flex";
+            printAns.innerHTML = printAns.innerHTML+ans;
+        }
     }
 }
 
+function handleRetry(){
+    let hideRetry = document.getElementById("result");
+    hideRetry.innerHTML = "";
+    hideRetry.style.display = "none";
+}
 function handleReplay(){window.location.reload()}
